5f26b2d89769c1bfae38d3a03a54b9a03329524c
Adrian Wright <awright@keeresources.com>
2021-03-22 18:59:14 +0000

    Add project files.

---
+++ VanillaBlazorWebAssemblyHostedWithAuth/Client/Attributes/AuthoriseByUserRolesAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Client.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Client.Attributes
+{
+    public class AuthoriseByUserRolesAttribute : AuthorizeAttribute
+    {
+        public AuthoriseByUserRolesAttribute(params UserRole[] userRoles)
+        {
+            if (userRoles.Length > 0)
+            {
+                Roles = string.Join(',', userRoles.Select(x => x.ToString()));
+            }
+        }
+    }
+}
---
+++ VanillaBlazorWebAssemblyHostedWithAuth/Client/Enums/UserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Client.Enums
+{
+    [Serializable]
+    public enum UserRole
+    {
+        [Description("Admin User")]
+        AdminUser = 'A',
+
+        [Description("Standard User")]
+        StandardUser = 'U',
+
+        [Description("Read-Only User")]
+        ReadOnlyUser = 'R'
+    }
+}
